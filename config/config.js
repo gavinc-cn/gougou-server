809c0e99bb22eb0b5de9db6f61cfe3a416746c3f
orca123456 <orca123456@163.com>
2018-11-18 23:16:05 +0800

    D:/Program Files/Git/

---
+++ config/config.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  qiniu: {
+    video: '你的七牛 video 域名地址',
+    AK: '你的七牛 AK',
+    SK: '你的七牛 SK'
+  },
+  cloudinary: {
+    cloud_name: '你的 cloudinary cloud_name',
+    api_key: '你的 cloud_name api_key',
+    api_secret: '你的 api_secret',
+    base: 'http://res.cloudinary.com/你的 cloud_name',
+    image: 'https://api.cloudinary.com/v1_1/你的 cloud_name/image/upload',
+    video: 'https://api.cloudinary.com/v1_1/你的 cloud_name/video/upload',
+    audio: 'https://api.cloudinary.com/v1_1/你的 cloud_name/raw/upload'
+  }
+}
